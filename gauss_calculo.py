694cf1fce48761b931cbb832675f4e060091fd50
Angelo D´Ambrosio <angelogdr2015@gmail.com>
2024-05-15 02:18:00 -0400

    version 2.0

--- gauss_calculo.py
+++ gauss_calculo.py
@@ -40,47 +40,24 @@ def main(page: ft.Page):
         # Llama a page.update() después de cambiar los valores
         page.update()
 
-    def gauss_seidel_modificado(A, b, max_iter=1000, tol=1e-8):
-        n = len(b)
-        x = np.zeros_like(b)
-        for it in range(max_iter):
-            x_prev = x.copy()
-            for i in range(n):
-                 x[i] = (b[i] - np.dot(A[i, :i], x[:i]) - np.dot(A[i, i+1:], x_prev[i+1:])) / A[i, i]
-            if np.linalg.norm(x - x_prev, ord=np.inf) / np.linalg.norm(x, ord=np.inf) < tol:
-                return x
-        return x
-
-    def on_click_solution(event):
-    # Crea una matriz A y un vector b a partir de los valores del GridView
-        matriz = np.array([float(control.value) for control in matrix_grid.controls]).reshape(3, 3)
-        b = matriz[:,-1]  # Puedes reemplazar esto con tu propio vector b
-
-    # Llama a la función gauss_seidel_modificado
-        x = gauss_seidel_modificado(matriz, b)
-
-        # Muestra la solución
-        for i, xi in enumerate(x):
-            page.add(ft.Text(f"x{i+1} = {xi}", text_align=ft.TextAlign.CENTER))
-
-    # Llama a page.update() después de agregar los nuevos controles
-        page.update()
-
-
     # Agrega los botones en una fila
     page.add(
-    ft.Row(
-        [
-            ft.ElevatedButton("Random", on_click=on_click_random),
-            ft.ElevatedButton("Solución", on_click=on_click_solution),
-        ],
-        alignment=ft.MainAxisAlignment.CENTER,
+        ft.Row(
+            [
+                ft.ElevatedButton("Random", on_click=on_click_random),  # Implementa la función para rellenar la matriz aleatoriamente
+                ft.ElevatedButton("Solución", on_click=None),  # Implementa la función para mostrar la solución
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
     )
-)
 
     page.update()
 
 ft.app(main)
+
+
+
+
 
 
 
@@ -98,13 +75,10 @@ def gauss_seidel_modificado(A, b, max_iter=1000, tol=1e-8):
     for it in range(max_iter):
         x_prev = x.copy()
         for i in range(n):
-            sum1 = np.dot(A[i, :i], x[:i]) if i else 0
-            sum2 = np.dot(A[i, i+1:], x_prev[i+1:]) if i < n-1 else 0
-            x[i] = (b[i] - sum1 - sum2) / A[i, i]
+            x[i] = (b[i] - np.dot(A[i, :i], x[:i]) - np.dot(A[i, i+1:], x_prev[i+1:])) / A[i, i]
         if np.linalg.norm(x - x_prev, ord=np.inf) / np.linalg.norm(x, ord=np.inf) < tol:
             return x
     return x
-
 
 
 
